c9a9b6d57b7dd92251fe405e8b8bd0828648f3f3
Guilherme <guilhermecarneiro@alunos.utfpr.edu.br>
2018-11-29 17:27:34 -0200

    adjusts and brain sql

--- commom/config.php
+++ commom/config.php
@@ -3,20 +3,24 @@
 class ConfigDatabase{
 
 	public function getConnection(){
-		$host = "localhost"; // Hostname
-		$port = "3306"; // MySQL Port : Default : 3306
-		$user = "root"; // Username Here
-		$pass = "brain123"; //Password Here
-		$db   = "brain"; // Database Name
+		try {
+			$host = "localhost"; // Hostname
+			$port = "3306"; // MySQL Port : Default : 3306
+			$user = "root"; // Username Here
+			$pass = "brain123"; //Password Here
+			$db   = "brain"; // Database Name
 
-		$dbh  = new PDO('mysql:dbname='.$db.';host='.$host.';port='.$port,$user,$pass);
+			$dbh  = new PDO('mysql:dbname='.$db.';host='.$host.';port='.$port,$user,$pass);
 
-		$dbh->query("SET NAMES 'utf8'");
-		$dbh->query('SET character_set_connection=utf8');
-		$dbh->query('SET character_set_client=utf8');
-		$dbh->query('SET character_set_results=utf8');
+			$dbh->query("SET NAMES 'utf8'");
+			$dbh->query('SET character_set_connection=utf8');
+			$dbh->query('SET character_set_client=utf8');
+			$dbh->query('SET character_set_results=utf8');
 
-		return $dbh;
+			return $dbh;
+		} catch (PDOException $e) {
+			echo "Error: " . $e->getMessage();
+		}
 	}
 
 	protected static function getDB() {
