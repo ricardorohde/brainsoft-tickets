003126bc5f1da4f6f1f917050566b1d8803f893c
Guilherme RC <guilherme.rcarneiro@gmail.com>
2019-03-15 08:48:26 -0300

    improved url request in current tickets and added structure of client and state

--- utils/pct-chat/api.php
+++ utils/pct-chat/api.php
@@ -2,235 +2,237 @@
 
 class ApiPct
 {
-	private $totalCustomersAtReception;
-	private $dataCustomersAtReception;
+    private $totalCustomersAtReception;
+    private $dataCustomersAtReception;
 
-	private $day;
-	private $month;
-	private $year;
-	private $idChatInSession;
-	private $ipClient;
-	private $attendant;
-	private $client;
-	private $start;
-	private $final;
-	private $rating;
-	private $transferedAt;
-	private $dataOfAllChats;
-	private $dataOfEspecificChat;
+    private $day;
+    private $month;
+    private $year;
+    private $idChatInSession;
+    private $ipClient;
+    private $attendant;
+    private $client;
+    private $start;
+    private $final;
+    private $rating;
+    private $transferedAt;
+    private $dataOfAllChats;
+    private $dataOfEspecificChat;
 
-	public function getTotalCustomersAtReception()
-	{
-	  return $this->totalCustomersAtReception;
-	}
+    public function getTotalCustomersAtReception()
+    {
+        return $this->totalCustomersAtReception;
+    }
 
-	public function getDataCustomersAtReception()
-	{
-	  return $this->dataCustomersAtReception;
-	}
+    public function getDataCustomersAtReception()
+    {
+        return $this->dataCustomersAtReception;
+    }
 
-	public function getDay() 
-	{
-		return $this->day;
-	}
+    public function getDay()
+    {
+        return $this->day;
+    }
 
-	public function getMonth() 
-	{
-		return $this->month;
-	}
+    public function getMonth()
+    {
+        return $this->month;
+    }
 
-	public function getYear() 
-	{
-		return $this->year;
-	}
+    public function getYear()
+    {
+        return $this->year;
+    }
 
-	public function getIdChatInSession() 
-	{
-		return $this->idChatInSession;
-	}
+    public function getIdChatInSession()
+    {
+        return $this->idChatInSession;
+    }
 
-	public function getIpClient() 
-	{
-		return $this->ipClient;
-	}
+    public function getIpClient()
+    {
+        return $this->ipClient;
+    }
 
-	public function getAttendant() 
-	{
-		return $this->attendant;
-	}
+    public function getAttendant()
+    {
+        return $this->attendant;
+    }
 
-	public function getClient()
-	{
-		return $this->client;
-	}
+    public function getClient()
+    {
+        return $this->client;
+    }
 
-	public function getStart()
-	{
-		return $this->start;
-	}
+    public function getStart()
+    {
+        return $this->start;
+    }
 
-	public function getFinal()
-	{
-		return $this->final;
-	}
+    public function getFinal()
+    {
+        return $this->final;
+    }
 
-	public function getRating()
-	{
-		return $this->rating;
-	}
+    public function getRating()
+    {
+        return $this->rating;
+    }
 
-	public function getDataOfAllChats() 
-	{
-		return $this->dataOfAllChats;
-	}
+    public function getDataOfAllChats()
+    {
+        return $this->dataOfAllChats;
+    }
 
-	public function getDataOfEspecificChat() 
-	{
-		return $this->dataOfEspecificChat;
-	}
+    public function getDataOfEspecificChat()
+    {
+        return $this->dataOfEspecificChat;
+    }
 
-	function __construct()
-	{
-		$this->defineTimezone();
-		$this->defineDay();
-		$this->defineMonth();
-		$this->defineYear();
-	}
+    function __construct()
+    {
+        $this->defineTimezone();
+        $this->defineDay();
+        $this->defineMonth();
+        $this->defineYear();
+    }
 
-	function defineTimezone()
-	{
-		date_default_timezone_set('America/Sao_Paulo');
-	}
+    function defineTimezone()
+    {
+        date_default_timezone_set('America/Sao_Paulo');
+    }
 
-	function consultCustomersAtReception()
-	{
-		$ch = curl_init();
-		$url = 'https://guilherme:aAoYdUycs71B5GfdfmqKRwaXUSr6iO50WiAuksHwbQzc7T4bH1eFVZvMBNqTG4px@brainsoft.meupct.com/api/chats';
+    function consultCustomersAtReception()
+    {
+        $actualDate = date('Y/m/d');
 
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $ch = curl_init();
+        $url = 'https://guilherme:aAoYdUycs71B5GfdfmqKRwaXUSr6iO50WiAuksHwbQzc7T4bH1eFVZvMBNqTG4px@brainsoft.meupct.com/api/chats/date/' . $actualDate;
 
-		$response = curl_exec($ch);
-		curl_close($ch);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-		$this->dataCustomersAtReception = json_decode($response);
-	}
+        $response = curl_exec($ch);
+        curl_close($ch);
 
-	function countCustomersAtReception()
-	{
-		$this->totalCustomersAtReception = 0;
+        $this->dataCustomersAtReception = json_decode($response);
+    }
 
-		foreach ($this->dataCustomersAtReception as $key => $value) {
-		    if ($value->chat_atendente == "camila" && $value->chat_final == NULL && $value->departamento == "Camila") {
-		    	$this->totalCustomersAtReception++;
-		    }
-		}
-	}
+    function countCustomersAtReception()
+    {
+        $this->totalCustomersAtReception = 0;
 
-	function toStringTotalCustomersAtReception()
-	{
-		$totalToString = "Nenhum Cliente";
+        foreach ($this->dataCustomersAtReception as $key => $value) {
+            if ($value->chat_atendente == "camila" && $value->chat_final == null && $value->departamento == "Camila") {
+                $this->totalCustomersAtReception++;
+            }
+        }
+    }
 
-		if ($this->totalCustomersAtReception == 1) {
-  			$totalToString = $this->totalCustomersAtReception . " Cliente";
-  		} 
+    function toStringTotalCustomersAtReception()
+    {
+        $totalToString = "Nenhum Cliente";
 
-  		if ($this->totalCustomersAtReception > 1) {
-  			$totalToString = $this->totalCustomersAtReception . " Clientes";
-  		}
-  		return $totalToString;
-	}
+        if ($this->totalCustomersAtReception == 1) {
+            $totalToString = $this->totalCustomersAtReception . " Cliente";
+        }
 
-	function defineDay()
-	{
-		$this->day = date('d');
-	}
+        if ($this->totalCustomersAtReception > 1) {
+            $totalToString = $this->totalCustomersAtReception . " Clientes";
+        }
+        return $totalToString;
+    }
 
-	function defineMonth()
-	{
-		$this->month = date('m');
-	}
+    function defineDay()
+    {
+        $this->day = date('d');
+    }
 
-	function defineYear()
-	{
-		$this->year = date('Y');
-	}
+    function defineMonth()
+    {
+        $this->month = date('m');
+    }
 
-	function assignIdChatInSession($id)
-	{
-		$_SESSION["id_chat"] = $id;
-		$this->idChatInSession = $id;
-	}
+    function defineYear()
+    {
+        $this->year = date('Y');
+    }
 
-	function convertData($time)
-	{
-		if ($time >= 60) {
-			$hour = floor($time / 60);
-	    	$seconds = $time % 60;
+    function assignIdChatInSession($id)
+    {
+        $_SESSION["id_chat"] = $id;
+        $this->idChatInSession = $id;
+    }
 
-	    	if (strlen($hour) < 2) {
-	      		$hour = "0" . $hour;
-	    	}
-	    	if (strlen($seconds) < 2) {
-	      		$seconds = "0" . $seconds;
-	    	}
-			return $hour . ':' . $seconds;
-	  	} else {
-	    	if (strlen($time) < 2) {
-	      		return "00:0" . $time;
-	    	} else {
-	      		return "00:" . $time;
-	    	}
-	  	}
-	}
+    function convertData($time)
+    {
+        if ($time >= 60) {
+            $hour = floor($time / 60);
+            $seconds = $time % 60;
 
-	function consultAllChats($date)
-	{
-		$ch1 = curl_init();
-		$ch2 = curl_init();
+            if (strlen($hour) < 2) {
+                $hour = "0" . $hour;
+            }
+            if (strlen($seconds) < 2) {
+                $seconds = "0" . $seconds;
+            }
+            return $hour . ':' . $seconds;
+        } else {
+            if (strlen($time) < 2) {
+                return "00:0" . $time;
+            } else {
+                return "00:" . $time;
+            }
+        }
+    }
 
-		$urlAllChats = 'https://guilherme:aAoYdUycs71B5GfdfmqKRwaXUSr6iO50WiAuksHwbQzc7T4bH1eFVZvMBNqTG4px@brainsoft.meupct.com/api/chats/date/' . $date;
-		$urlEspecificChat = 'https://guilherme:aAoYdUycs71B5GfdfmqKRwaXUSr6iO50WiAuksHwbQzc7T4bH1eFVZvMBNqTG4px@brainsoft.meupct.com/api/chats/'.$this->idChatInSession; 
+    function consultAllChats($date)
+    {
+        $ch1 = curl_init();
+        $ch2 = curl_init();
 
-		curl_setopt($ch1, CURLOPT_URL, $urlAllChats);
-		curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+        $urlAllChats = 'https://guilherme:aAoYdUycs71B5GfdfmqKRwaXUSr6iO50WiAuksHwbQzc7T4bH1eFVZvMBNqTG4px@brainsoft.meupct.com/api/chats/date/' . $date;
+        $urlEspecificChat = 'https://guilherme:aAoYdUycs71B5GfdfmqKRwaXUSr6iO50WiAuksHwbQzc7T4bH1eFVZvMBNqTG4px@brainsoft.meupct.com/api/chats/' . $this->idChatInSession;
 
-		curl_setopt($ch2, CURLOPT_URL, $urlEspecificChat);
-		curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch1, CURLOPT_URL, $urlAllChats);
+        curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
 
-		$mh = curl_multi_init();
+        curl_setopt($ch2, CURLOPT_URL, $urlEspecificChat);
+        curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
 
-		curl_multi_add_handle($mh, $ch1);
-		curl_multi_add_handle($mh, $ch2);
-		  
-		$running = null;
+        $mh = curl_multi_init();
 
-		do {
-		    curl_multi_exec($mh, $running);
-		} while ($running);
+        curl_multi_add_handle($mh, $ch1);
+        curl_multi_add_handle($mh, $ch2);
 
-		curl_multi_remove_handle($mh, $ch1);
-		curl_multi_remove_handle($mh, $ch2);
-		curl_multi_close($mh);
-		  
-		$responseOfAllChats = curl_multi_getcontent($ch1);
-		$responseOfEspecificChat = curl_multi_getcontent($ch2);
+        $running = null;
 
-		$this->dataOfAllChats = json_decode($responseOfAllChats);
-		$this->dataOfEspecificChat = json_decode($responseOfEspecificChat);
-	}
+        do {
+            curl_multi_exec($mh, $running);
+        } while ($running);
 
-	function putFeaturesOfChatInVariables()
-	{
-		foreach ($this->dataOfAllChats as $key => $value) {
-		    if ($value->cod_chat == $this->idChatInSession) {
-		    	$this->ipClient = $value->cliente_ip;
-		      	$this->attendant = $value->chat_atendente;
-		      	$this->client = $value->cliente_nome;
-		      	$this->start = $value->chat_inicio;
-		      	$this->final = $value->chat_final;
-		      	$this->rating = $value->chat_nota_atendimento;
-		    }
-		}
-	}
+        curl_multi_remove_handle($mh, $ch1);
+        curl_multi_remove_handle($mh, $ch2);
+        curl_multi_close($mh);
+
+        $responseOfAllChats = curl_multi_getcontent($ch1);
+        $responseOfEspecificChat = curl_multi_getcontent($ch2);
+
+        $this->dataOfAllChats = json_decode($responseOfAllChats);
+        $this->dataOfEspecificChat = json_decode($responseOfEspecificChat);
+    }
+
+    function putFeaturesOfChatInVariables()
+    {
+        foreach ($this->dataOfAllChats as $key => $value) {
+            if ($value->cod_chat == $this->idChatInSession) {
+                $this->ipClient = $value->cliente_ip;
+                $this->attendant = $value->chat_atendente;
+                $this->client = $value->cliente_nome;
+                $this->start = $value->chat_inicio;
+                $this->final = $value->chat_final;
+                $this->rating = $value->chat_nota_atendimento;
+            }
+        }
+    }
 }
